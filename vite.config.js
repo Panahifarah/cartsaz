d6e518d5da397d78f6d9a6b8af73bbc657cca1cc
Amir Husayn Panahifar <Panahifar.ah@outlook.com>
2024-09-03 11:59:06 +0330

    Add vitest for testing support in react app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { config } from "dotenv";
 
-// https://vitejs.dev/config/
+config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000,
+  },
+});

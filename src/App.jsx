4ebed5ffac9f993ba779814cb3d74bee2378d113
Amir Husayn Panahifar <Panahifar.ah@outlook.com>
2024-09-03 12:09:18 +0330

    Add app routing with react-router-dom and global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./utils/routes/routesConfig";
+import "./styles/globals.sass";
 
 const App = () => {
-
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
